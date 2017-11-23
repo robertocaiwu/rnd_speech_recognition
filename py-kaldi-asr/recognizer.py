a767fc117ebb7ab5a298989a3013630fb79a060e
Roberto Cai <robertocw>
2017-11-23 17:24:18 +0100

    add config jenny py-kaldi wrapper

--- py-kaldi-asr/recognizer.py
+++ py-kaldi-asr/recognizer.py
@@ -1,16 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8 
 
+import pyaudio
+import wave
 from kaldiasr.nnet3 import KaldiNNet3OnlineModel, KaldiNNet3OnlineDecoder
 
-MODELDIR    = 'data/models/kaldi-nnet3-voxforge-en-r20171030'
+MODELDIR    = 'data/models/nnet3_de'
 MODEL       = 'nnet_tdnn_a'
-WAVFILES    = [ 'data/a0405.wav', 'data/a0406.wav', 'data/a0407.wav']
+WAVFILES    = [ 'data/2015-01-27-12-32-58_Kinect-Beam.wav', 'data/2015-01-27-12-34-46_Kinect-Beam.wav']
 
-print '%s loading model...' % MODEL
-kaldi_model = KaldiNNet3OnlineModel (MODELDIR, MODEL)
-print '%s loading model... done.' % MODEL
+# Options
+CHUNK = 128 # The size of each audio chunk coming from the input device.
+FORMAT = pyaudio.paInt16 # Should not be changed, as this format is best for speech recognition.
+CHANNELS = 1
+RATE = 16000 # Speech recognition only works well with this rate.  Don't change unless your microphone demands it.
+RECORD_SECONDS = 8 # Number of seconds to record, can be changed.
+WAVE_OUTPUT_FILENAME = "output.wav" # Where to save the recording from the microphone.
 
-decoder = KaldiNNet3OnlineDecoder (kaldi_model)
-for WAVFILE in WAVFILES:
-	if decoder.decode_wav_file(WAVFILE):
-	    s = decoder.get_decoded_string()
-	    print "Utterance: ", s
+def save_audio(wav_file):
+	"""
+	Stream audio from an input device and save it.
+	"""
+	p = pyaudio.PyAudio()
+
+	stream = p.open(
+		format=FORMAT,
+		channels=CHANNELS,
+		rate=RATE,
+		input=True,
+		frames_per_buffer=CHUNK
+		# input_device_index=device
+	)
+
+	print("recording...")
+
+	frames = []
+
+	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+		data = stream.read(CHUNK)
+		frames.append(data)
+
+	print("* done recording")
+
+	stream.stop_stream()
+	stream.close()
+
+	p.terminate()
+
+	wf = wave.open(DATADIR + wav_file, 'wb')
+	wf.setnchannels(CHANNELS)
+	wf.setsampwidth(p.get_sample_size(FORMAT))
+	wf.setframerate(RATE)
+	wf.writeframes(b''.join(frames))
+	wf.close()
+
+# Run the thing!
+if __name__ == '__main__':
+	text = raw_input("What language do you want to recognize? en/de")
+
+	if(text=='en'):
+                MODELDIR    = 'data/models/nnet3_en'
+		print '%s loading model...' % MODEL
+		kaldi_model = KaldiNNet3OnlineModel (MODELDIR, MODEL)
+		print '%s loading model... done.' % MODEL
+	elif(text=='de'):
+		MODELDIR    = 'data/models/nnet3_de'
+		print '%s loading model...' % MODEL
+		kaldi_model = KaldiNNet3OnlineModel (MODELDIR, MODEL)
+		print '%s loading model... done.' % MODEL		
+		
+
+	while(True):
+		text = raw_input("Test speech recognition? y/n")
+		if(text=='y'):
+			save_audio(WAVE_OUTPUT_FILENAME)
+			decoder = KaldiNNet3OnlineDecoder (kaldi_model)
+			WAVFILE = DATADIR + WAVE_OUTPUT_FILENAME
+			if decoder.decode_wav_file(WAVFILE):
+				s = decoder.get_decoded_string()
+				print "Utterance: ", unicode(s[0], 'utf-8').encode('utf-8')
+		elif(text=='n'):
+			break
+		elif(text=='l'):
+			WAVFILES    = [ 'data/2015-01-27-12-32-58_Kinect-Beam.wav', \
+					'data/2015-01-27-12-34-36_Kinect-Beam.wav', \
+					'data/2015-01-27-12-34-46_Kinect-Beam.wav']
+			decoder = KaldiNNet3OnlineDecoder (kaldi_model)
+			if decoder.decode_wav_file(WAVFILES[0]):
+				s = decoder.get_decoded_string()
+				print "Utterance: ", unicode(s[0], 'utf-8').encode('utf-8')
--- py-kaldi-asr/setup.py
+++ py-kaldi-asr/setup.py
@@ -1,6 +1,6 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 import numpy
+import commands
 
 try:
     from Cython.Distutils import build_ext
@@ -8,42 +8,53 @@ except ImportError:
     use_cython = False
 else:
     use_cython = True
+
 cmdclass = { }
 ext_modules = [ ]
 
+def pkgconfig(*packages, **kw):
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages)).split():
+        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return kw
+
+#CFLAGS = -Wall -pthread -std=c++11 -DKALDI_DOUBLEPRECISION=0 -Wno-sign-compare \
+#         -Wno-unused-local-typedefs -Winit-self -DHAVE_EXECINFO_H=1 -DHAVE_CXXABI_H -DHAVE_ATLAS \
+#         `pkg-config --cflags kaldi-asr` -g
+print (use_cython)
 if use_cython:
     ext_modules += [
         Extension("kaldiasr.nnet3", 
                   sources  = [ "kaldiasr/nnet3.pyx", "kaldiasr/nnet3_wrappers.cpp" ],
-                  language = "c++",
-                  extra_compile_args=['-std=gnu++11']),
+                  language = "c++", 
+                  extra_compile_args = [ '-Wall', '-pthread', '-std=c++11', '-DKALDI_DOUBLEPRECISION=0', '-Wno-sign-compare', '-Wno-unused-local-typedefs', '-Winit-self', '-DHAVE_EXECINFO_H=1', '-DHAVE_CXXABI_H', '-DHAVE_ATLAS', '-g'  ],
+                  **pkgconfig('kaldi-asr')),
     ]
     cmdclass.update({ 'build_ext': build_ext })
 else:
     ext_modules += [
         Extension("kaldiasr.nnet3", 
-                  sources  = [ "kaldiasr/nnet3.cpp", "kaldiasr/nnet3_wrappers.cpp" ],
-                  language = "c++",),
+                  sources  = [ "kaldiasr/nnet3.pyx", "kaldiasr/nnet3_wrappers.cpp" ],
+		  language = "c++",
+                  extra_compile_args = [ '-Wall', '-pthread', '-std=c++11', '-DKALDI_DOUBLEPRECISION=0', '-Wno-sign-compare', '-Wno-unused-local-typedefs', '-Winit-self', '-DHAVE_EXECINFO_H=1', '-DHAVE_CXXABI_H', '-DHAVE_ATLAS', '-g'  ],
+                  **pkgconfig('kaldi-asr')),
     ]
 
 setup(
     name                 = 'py-kaldi-asr',
-    version              = '0.1.0',
-    description          = 'Simple Python/Cython interface to kaldi-asr decoders',
+    version              = '0.2.0',
+    description          = 'Simple Python/Cython interface to kaldi-asr nnet3/chain decoders',
     long_description     = open('README.md').read(),
     author               = 'Guenter Bartsch',
     author_email         = 'guenter@zamia.org',
     maintainer           = 'Guenter Bartsch',
     maintainer_email     = 'guenter@zamia.org',
-    url                  = 'https://github.com/gooofy/py-kaldi-simple',
-    # download_url         = 'https://pypi.python.org/pypi/kaldisimple',
+    url                  = 'https://github.com/gooofy/py-kaldi-asr',
     packages             = ['kaldiasr'],
     cmdclass             = cmdclass,
     ext_modules          = ext_modules,
-    # include_dirs         = [numpy.get_include(), '/home/rob/speech_toolkit/kaldi/src', '/home/rob/speech_toolkit/kaldi/tools/openfst/include', '/home/rob/speech_toolkit/kaldi/src/ATLAS/include'],
     include_dirs         = [numpy.get_include()],
     classifiers          = [
-                               'Development Status :: 2 - Pre-Alpha',
                                'Operating System :: POSIX :: Linux',
                                'License :: OSI Approved :: Apache Software License',
                                'Programming Language :: Python :: 2',
@@ -55,22 +66,6 @@ setup(
                                'Topic :: Multimedia :: Sound/Audio :: Speech'
                            ],
     license              = 'Apache',
-    keywords             = 'kaldi-asr',
-    # test_suite           = 'tests', 
-    # include_package_data = True,
-    # zip_safe             = False
+    keywords             = 'kaldi asr',
     )
 
-# FIXME: remove old code
-# from distutils.core import setup
-# from Cython.Build import cythonize
-# import numpy
-# 
-# setup(ext_modules = cythonize(
-#            "kaldisimple/kaldi_simple.pyx",               
-#            sources  = ["kaldisimple/KaldiSimple.cpp"],  
-#            language = "c++",
-#       ),
-#       include_dirs = [numpy.get_include()]
-#      )
-#
