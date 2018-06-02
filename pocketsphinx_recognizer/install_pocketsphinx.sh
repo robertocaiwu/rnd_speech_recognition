27197cf29b82898596a631c5b9fd33409c4f0187
rob <robertocw_18@hotmail.com>
2018-06-02 12:28:03 +0200

    updated pocketsphinx install script 1.0

--- pocketsphinx_recognizer/install_pocketsphinx.sh
+++ pocketsphinx_recognizer/install_pocketsphinx.sh
@@ -26,7 +26,8 @@ packagelist=(
 sudo apt-get update
 sudo apt-get install -y ${packagelist[@]}
 
-
+# Assumes you already have the development version of the Python
+# package for your distribution.
 if [ ! -d sphinxbase ]
 then
   git clone git@github.com:cmusphinx/sphinxbase.git
@@ -34,9 +35,15 @@ fi
 cd sphinxbase
 ./autogen.sh
 ./configure
+make clean
 make check
 sudo make install
 cd ..
+
+echo 'export PKG_CONFIG_PATH=~/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=~/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+
+source ~/.bashrc
 
 if [ ! -d pocketsphinx ]
 then
@@ -46,10 +53,6 @@ fi
 cd pocketsphinx
 ./autogen.sh
 ./configure
+make clean
 make check
 sudo make install
-
-echo 'export PKG_CONFIG_PATH=~/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=~/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
-
-source ~/.bashrc
